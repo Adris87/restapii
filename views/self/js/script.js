6b9eb60a10ed1038887d947101feadb501defe95
wannofc <93016847+wannofc@users.noreply.github.com>
2022-06-26 19:52:40 +0700

    Delete views/self directory

--- views/self/js/script.js
+++
@@ -1,8 +0,0 @@
-    window.setTimeout("waktu()", 1000);
-    function waktu() {
-        var waktu = new Date();
-        setTimeout("waktu()", 1000);
-        const stringss = `${waktu.getHours()}:${waktu.getMinutes()}:${waktu.getSeconds()}`
-        document.getElementById("jam").textContent = stringss
-        
-        }
